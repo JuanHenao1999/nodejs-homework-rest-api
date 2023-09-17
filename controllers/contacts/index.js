b43d50b1727bc24d6a3d2b4c37b698ea05067ad8
JuanHenao1999 <juanhenao123kl@gmail.com>
2023-09-17 12:41:23 -0500

    two commit

---
+++ controllers/contacts/index.js
@@ -0,0 +1,16 @@
+const getAll = require("./getAll");
+const getById = require("./getById");
+const addContact = require("./addContacts");
+const updateById = require("./updateById");
+const updateFavorite = require("./updateFavorite");
+const removeById = require("./removeById");
+
+
+module.exports = {
+    getAll,
+    getById,
+    addContact,
+    updateById,
+    updateFavorite,
+    removeById,
+}
